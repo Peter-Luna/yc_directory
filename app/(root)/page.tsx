13d17d77a1c1803811ad210d5183ce749e079252
lph1545 <1545985939@qq.com>
2025-02-21 20:21:25 +0800

    制作好卡片的展示并添加了一个测试数据

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import StartupCard from '@/components/StartupCard';
 import SearchForm from '../../components/SearchForm';
 
 export default async function Home({
@@ -6,6 +7,20 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query || '';
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: 'Peter' },
+      _id: 1,
+      description: 'This is a description',
+      image:
+        'https://s3.vct.news/uploads/2023060201064/0e69446a54f9dc68ad6ee224c1edbbe71a45587777e95e324a8c02d1e2f8283d.jpg',
+      category: 'Robots',
+      title: 'We Robots',
+    },
+  ];
 
   return (
     <>
@@ -18,6 +33,20 @@ export default async function Home({
           Competitions.
         </p>
         <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : 'All Startups'}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
