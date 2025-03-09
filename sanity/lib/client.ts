c16ae061585d2fb34db3b67676b681d987a28e52
lph1545 <1545985939@qq.com>
2025-03-09 22:50:44 +0800

    创建页面的搭建

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -6,6 +6,6 @@ export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: false, //  Set to false if statically generating pages, using ISR or tag-based revalidation
+  useCdn: true, //  Set to false if statically generating pages, using ISR or tag-based revalidation
 });
 // 如果使用 ISR 或基于标签的重新验证静态生成页面，则设置为 false
